fa67b0449f4f5ecc14ac05f9ca0a8aa20dde230e
Rafael Corrêa de Melo <rfael403@gmail.com>
2020-05-12 21:15:28 -0300

    adicionado formulario de client

--- ProjetoSalaoDeBeleza/FormAreaUsuario.cs
+++ ProjetoSalaoDeBeleza/FormAreaUsuario.cs
@@ -16,5 +16,20 @@ namespace ProjetoSalaoDeBeleza
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
